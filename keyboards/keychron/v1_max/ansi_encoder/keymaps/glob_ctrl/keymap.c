7d797b911539f45c48c1a89c597d795485892657
Max <>
2026-03-30 15:48:04 +1300

    Make Right Control be Glob button

--- keyboards/keychron/v1_max/ansi_encoder/keymaps/glob_ctrl/keymap.c
+++ keyboards/keychron/v1_max/ansi_encoder/keymaps/glob_ctrl/keymap.c
@@ -18,10 +18,8 @@
 #include "keychron_common.h"
 
 enum custom_keycodes {
-    CTL_GLOB = SAFE_RANGE,
+    KC_GLOB = SAFE_RANGE,
 };
-
-static uint16_t glob_timer;
 
 enum layers {
     MAC_BASE,
@@ -41,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, FN_MAC,  CTL_GLOB,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, FN_MAC,   KC_GLOB,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_82(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            UG_TOGG,
@@ -80,18 +78,11 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case CTL_GLOB:
+        case KC_GLOB:
             if (record->event.pressed) {
-                glob_timer = timer_read();
-                register_code(KC_RCTL); // Act as Control immediately on press
+                host_consumer_send(AC_NEXT_KEYBOARD_LAYOUT_SELECT);
             } else {
-                unregister_code(KC_RCTL); // Release Control
-                
-                // If it was a quick tap, send the Globe key
-                if (timer_elapsed(glob_timer) < TAPPING_TERM) {
-                    host_consumer_send(AC_NEXT_KEYBOARD_LAYOUT_SELECT);
-                    host_consumer_send(0);
-                }
+                host_consumer_send(0);
             }
             return false;
     }
